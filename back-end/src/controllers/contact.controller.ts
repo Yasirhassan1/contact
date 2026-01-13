0dc17e878246bb5a2c4d694b8b2bd8dbdf5d09f0
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-13 15:51:01 +0500

    optimize the file structure

---
+++ back-end/src/controllers/contact.controller.ts
@@ -0,0 +1,119 @@
+import { type Request, type Response } from "express";
+import contactModel from "../models/contact.model.ts";
+
+export const  getAllContacts = async (req:Request, res:Response)=>{
+  try{
+    let contacts = await contactModel.find();
+    res.json(
+  contacts.map(c => ({
+    id: c._id,
+    name: c.name,
+    phoneNo: c.phoneNo,
+    email: c.email,
+    createdAt: c.createdAt,
+  }))
+);
+  }
+  catch(err){
+    console.log(err)
+    res.json({
+      success:false
+    })
+  }
+    
+}
+
+
+export const createContact = async (req:Request, res:Response)=>{
+    const {name, phoneNo, email} = req.body;
+    try{
+await contactModel.create({
+  name: name,
+  phoneNo: phoneNo,
+  email: email,
+});
+
+let contacts = await contactModel.find()
+   res.json({
+      contacts,
+        success:true
+    })
+    }
+    catch(err){
+      console.log(err)
+       res.json({
+        success:false
+    })
+    }
+    
+ 
+ 
+}
+
+export const deleteContact = async (req:Request, res:Response)=>{
+try{
+    await contactModel.findByIdAndDelete(req.params.id);
+    let contacts = await contactModel.find()
+
+    res.json({contacts, success:true})
+}
+catch(err){
+  console.log(err)
+  res.json({success:false})
+}
+
+}
+
+
+export const updateContact = async (req:Request, res:Response)=>{
+    try{
+    const {name, phoneNo, email} = req.body;
+   await contactModel.findByIdAndUpdate(
+  req.params.id,
+  {
+    name: name,
+    phoneNo: phoneNo,
+    email:email
+  },
+res.json({success:true})
+);
+    
+}
+catch(err){
+  console.log(err)
+  res.json({success:false})
+}
+}
+
+export const searchContact = async (req: Request, res: Response) => {
+  try {
+    const character = req.params.character;
+let contacts = await contactModel.find();
+
+    if (!character) {
+      return res.json({ success: false, id: null });
+    }
+
+
+    const search = character.toLowerCase();
+
+    const targetIds = contacts.filter((cur) =>
+      cur.name.toLowerCase().includes(search)
+    )?.map((cur)=> cur.id);
+
+    if (targetIds == null) {
+      return res.json({
+        success: false,
+        id: null,
+      });
+    }
+
+    res.json({
+      success: true,
+      ids: targetIds,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false });
+  }
+}
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,147 +1,36 @@
-import express, { type Request, type Response } from "express";
-import cors from "cors";
+import express from "express";
 import dotenv from 'dotenv'
 import connectDB from "./dbConnection/dbConnection.ts";
-import contactModel from "./models/contact.model.ts";
+import { createContact, deleteContact, getAllContacts, searchContact, updateContact } from "./controllers/contact.controller.ts";
+import { corsMiddleware } from "./middlewares/cors.middleware.ts";
+
 dotenv.config()
+
 const app = express();
 
-app.use(cors({
-  origin: [
-    "https://contact-3k4v.vercel.app",
-    "http://localhost:3000"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+app.use(corsMiddleware);
+
 app.use(express.json());
 
 
-
 const PORT = Number(process.env.PORT) ||5000;
-connectDB()
 
-app.get("/", async (req:Request, res:Response)=>{
-  try{
-    let contacts = await contactModel.find();
-    res.json(
-  contacts.map(c => ({
-    id: c._id,
-    name: c.name,
-    phoneNo: c.phoneNo,
-    email: c.email,
-    createdAt: c.createdAt,
-  }))
-);
-  }
-  catch(err){
-    console.log(err)
-    res.json({
-      success:false
-    })
-  }
+connectDB();
 
-    
-});
+app.post("/create", createContact);
 
-app.post("/create", async (req:Request, res:Response)=>{
-    const {name, phoneNo, email} = req.body;
-    try{
-await contactModel.create({
-  name: name,
-  phoneNo: phoneNo,
-  email: email,
-});
+app.get("/", getAllContacts);
 
-let contacts = await contactModel.find()
-   res.json({
-      contacts,
-        success:true
-    })
-    }
-    catch(err){
-      console.log(err)
-       res.json({
-        success:false
-    })
-    }
-    
- 
- 
-}
+app.delete("/delete/:id",  deleteContact);
 
-)
+app.put("/edit/:id",  updateContact);
+
+
+app.get("/search/:character", searchContact);
 
 
 
-app.delete("/delete/:id",  async (req:Request, res:Response)=>{
-try{
-    await contactModel.findByIdAndDelete(req.params.id);
-    let contacts = await contactModel.find()
-
-    res.json({contacts, success:true})
-}
-catch(err){
-  console.log(err)
-  res.json({success:false})
-}
-
+app.listen(PORT, ()=>{
+    console.log("App is running on port ", PORT)
 })
-
-app.put("/edit/:id",  async (req:Request, res:Response)=>{
-    try{
-    const {name, phoneNo, email} = req.body;
-   await contactModel.findByIdAndUpdate(
-  req.params.id,
-  {
-    name: name,
-    phoneNo: phoneNo,
-    email:email
-  },
-res.json({success:true})
-);
-    
-}
-catch(err){
-  console.log(err)
-  res.json({success:false})
-}
-}
-)
-
-
-app.get("/search/:character", async (req: Request, res: Response) => {
-  try {
-    const character = req.params.character;
-let contacts = await contactModel.find();
-
-    if (!character) {
-      return res.json({ success: false, id: null });
-    }
-
-
-    const search = character.toLowerCase();
-
-    const targetIds = contacts.filter((cur) =>
-      cur.name.toLowerCase().includes(search)
-    )?.map((cur)=> cur.id);
-
-    if (targetIds == null) {
-      return res.json({
-        success: false,
-        id: null,
-      });
-    }
-
-    res.json({
-      success: true,
-      ids: targetIds,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ success: false });
-  }
-});
-
-export default app;
 

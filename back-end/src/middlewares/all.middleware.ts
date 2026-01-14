09b6d380c6f6b4bcb134baefcac80e0884ca4019
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-14 19:20:06 +0500

    simple

--- back-end/src/middlewares/all.middleware.ts
+++ back-end/src/middlewares/all.middleware.ts
@@ -1,10 +1,4 @@
 import express, {type Express}  from "express";
-import { writeLog } from "../middlewares/log.middleware.ts";
-import { rateLimmiter } from "../middlewares/limmiter.middleware.ts";
-// import helmet from "helmet";
 export default function runAllMiddleware(app:Express){
-    // app.use(helmet())
     app.use(express.json());
-    app.use(writeLog);
-    app.use(rateLimmiter);
 }
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,10 +1,8 @@
 import express from "express";
 import dotenv from 'dotenv'
-import connectDB from "./dbConnection/dbConnection.ts";
-import { createContact, deleteContact, getAllContacts, searchContact, updateContact } from "./controllers/contact.controller.ts";
 import runAllMiddleware from "./middlewares/all.middleware.ts";
-import { type Request, type Response } from "express"
 
+import type { Request, Response } from "express";
 dotenv.config()
 
 const app = express();
@@ -15,18 +13,13 @@ runAllMiddleware(app)
 const PORT = Number(process.env.PORT) ||5000;
 
 
-app.post("/create", createContact);
-
 app.get("/", (req:Request, res:Response)=>{
-    res.json({message:"ello"})
-});
-
-app.delete("/delete/:id",  deleteContact);
-
-app.put("/edit/:id",  updateContact);
-
-
-app.get("/search/:character", searchContact);
+    res.json({
+        name:"Yasir",
+        semester:"6th",
+        gender:"male"
+    })
+})
 
 
 

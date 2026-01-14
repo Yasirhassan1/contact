0d990152755e9ec06669550b0ce3fab8159e45df
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-14 20:20:17 +0500

    fixx

--- back-end/src/dbConnection/dbConnection.ts
+++ back-end/src/dbConnection/dbConnection.ts
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
-import dotenv from 'dotenv'
 
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
 
 dotenv.config()
 let isConnected = false; // simple flag

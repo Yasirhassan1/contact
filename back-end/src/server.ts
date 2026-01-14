09c1d83d88385c0b272f611997aae3eea6f38353
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-14 21:13:33 +0500

    ldkjfkljdslkhf

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from 'dotenv'
 import connectDB from "./dbConnection/dbConnection.js";
 import { getAllContacts } from "./controllers/contact.controller.js";
+import { corsMiddleware } from "./middlewares/cors.middleware.js";
 
 dotenv.config()
 
 const app = express();
 const PORT = Number(process.env.PORT) ||5000;
 app.use(express.json());
+app.use(corsMiddleware)
 
 
 connectDB();

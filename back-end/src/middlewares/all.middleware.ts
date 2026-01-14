ceea8d8ecec59169fce871b8a31cb89c07652ee1
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-14 20:59:59 +0500

    hellrrr

--- back-end/src/middlewares/all.middleware.ts
+++ back-end/src/middlewares/all.middleware.ts
@@ -1,4 +1,6 @@
 import express, {type Express}  from "express";
+import { corsMiddleware } from "./cors.middleware.js";
 export default function runAllMiddleware(app:Express){
     app.use(express.json());
+    app.use(corsMiddleware);
 }

71f033f499df85c7620b6c0207604c50b8cee9c3
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-14 16:54:34 +0500

    added helmet package

--- back-end/src/middlewares/log.middleware.ts
+++ back-end/src/middlewares/log.middleware.ts
@@ -1,7 +1,7 @@
 import { type NextFunction, type Request, type Response } from "express"
 import { appendFile } from "node:fs"
 export const  writeLog  = (req:Request, res:Response, next:NextFunction)=>{ 
-    appendFile("log.txt",`\n IP: ${req.ip} Method: ${req.method} Time: ${String(new Date())}`, ()=>{
+    appendFile("log.txt",`IP: ${req.ip} Method: ${req.method} Time: ${String(new Date())}\n`, ()=>{
 
     })
     next()
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,21 +2,14 @@ import express from "express";
 import dotenv from 'dotenv'
 import connectDB from "./dbConnection/dbConnection.ts";
 import { createContact, deleteContact, getAllContacts, searchContact, updateContact } from "./controllers/contact.controller.ts";
-import { corsMiddleware } from "./middlewares/cors.middleware.ts";
-import { writeLog } from "./middlewares/log.middleware.ts";
-import { rateLimmiter } from "./middlewares/limmiter.middleware.ts";
+import runAllMiddleware from "./middlewares/all.middleware.ts";
+
 
 dotenv.config()
 
 const app = express();
 
-app.use(corsMiddleware);
-
-app.use(express.json());
-
-app.use(writeLog);
-app.use(rateLimmiter);
-
+runAllMiddleware(app)
 
 const PORT = Number(process.env.PORT) ||5000;
 

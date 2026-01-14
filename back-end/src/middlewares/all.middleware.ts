89cd741d5a99e5313db7958f5fc9be29a8ae1bdf
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-14 19:09:17 +0500

    change

--- back-end/src/middlewares/all.middleware.ts
+++ back-end/src/middlewares/all.middleware.ts
@@ -1,9 +1,9 @@
 import express, {type Express}  from "express";
 import { writeLog } from "../middlewares/log.middleware.ts";
 import { rateLimmiter } from "../middlewares/limmiter.middleware.ts";
-import helmet from "helmet";
+// import helmet from "helmet";
 export default function runAllMiddleware(app:Express){
-    app.use(helmet())
+    // app.use(helmet())
     app.use(express.json());
     app.use(writeLog);
     app.use(rateLimmiter);

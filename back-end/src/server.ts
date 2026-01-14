9ae5040b4ee5555bec74964089df4225de22b839
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-14 18:59:27 +0500

    changes

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -3,31 +3,23 @@ import dotenv from 'dotenv'
 import connectDB from "./dbConnection/dbConnection.ts";
 import { createContact, deleteContact, getAllContacts, searchContact, updateContact } from "./controllers/contact.controller.ts";
 import runAllMiddleware from "./middlewares/all.middleware.ts";
-import cors from "cors";
+import { type Request, type Response } from "express"
 
 dotenv.config()
 
 const app = express();
 
+
 runAllMiddleware(app)
 
 const PORT = Number(process.env.PORT) ||5000;
 
-connectDB();
-let whitelist = ['https://contact-3k4v.vercel.app', 'http://localhost:3000']
-let corsOptions = {
-  origin: function (origin:any, callback:any) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
 
 app.post("/create", createContact);
 
-app.get("/", cors(corsOptions), getAllContacts);
+app.get("/", (req:Request, res:Response)=>{
+    res.json({message:"hello"})
+});
 
 app.delete("/delete/:id",  deleteContact);
 

ab2172856bebf18583ddf5e6a3fce300e0dbe37c
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-15 18:33:36 +0500

    added helmet middleware

--- back-end/src/middlewares/all.middleware.ts
+++ back-end/src/middlewares/all.middleware.ts
@@ -2,7 +2,9 @@ import express, {type Express}  from "express";
 import { corsMiddleware } from "./cors.middleware.js";
 import { writeLog } from "./log.middleware.js";
 import { rateLimmiter } from "./limmiter.middleware.js";
+import helmet from "helmet";
 export default function runAllMiddleware(app:Express){
+    app.use(helmet())
     app.use(express.json());
     app.use(corsMiddleware);
     app.use(writeLog);

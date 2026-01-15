77d464a6490c082685c5ff7310204e742ef9a962
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-15 08:54:00 +0500

    chnage the tsconig

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -7,27 +7,16 @@ import runAllMiddleware from "./middlewares/all.middleware.js";
 dotenv.config()
 
 const app = express();
-const PORT = Number(process.env.PORT) ||5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 runAllMiddleware(app)
 
 connectDB();
-
-
-
 app.post("/create", createContact);
-
 app.get("/", getAllContacts);
-
 app.delete("/delete/:id",  deleteContact);
-
 app.put("/edit/:id",  updateContact);
-
-
 app.get("/search/:character", searchContact);
-
-
-
 app.listen(PORT, ()=>{
     console.log("App is running on port ", PORT)
 })

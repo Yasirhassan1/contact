c3beec8a21c8c87a2063c037194071712bbeba62
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-15 19:58:47 +0500

    added hooks

---
+++ front-end/app/hooks/useContact.ts
@@ -0,0 +1,155 @@
+import { useState, useEffect } from "react";
+import { Contact } from "../types/types";
+import axios from "axios";
+export const useContact = ()=>{
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL as string;
+      const [contacts, setContacts]  = useState<Contact[]>([])
+      const [showForm, setShowForm] = useState<boolean>(false)
+      const [isEdit, setIsEdit] = useState<boolean>(false)
+      const [editId, setEditId] = useState<string>("")
+      const [searchName, setSearchName] = useState<string>("")
+      const [searchIds, setSearchIds] = useState<string[] | null>([]) 
+      const [isSearched, setIsSearched] = useState<boolean>(false)
+      const [isLoading, setIsLoading] = useState<boolean>(true)
+       const [form, setForm] = useState<Contact>({
+  _id: "",
+  name: "",
+  phoneNo: "",
+  email: ""
+});
+
+
+      function getAllContacts(){
+        axios.get(`${apiUrl}/`)
+       .then((response)=>{
+        setContacts(response.data.contacts)
+        setIsLoading(false);
+       })
+       .catch((err)=>{
+        console.log(err)
+       })
+      }
+      
+      function deleteContact(id:string){
+        axios.delete(`${apiUrl}/delete/${id}`)
+        .then((response)=>{
+          setContacts(response.data.contacts)
+          setIsLoading(false)
+          
+        })
+         .catch(function (error) {
+          console.log(error);
+        });
+      }
+      
+      function searchContact(name: string) {
+        if(name){
+      
+      setIsSearched(true)
+        axios
+          .get(`${apiUrl}/search/${name}`)
+          .then((response) => {
+            
+            if (response.data.success) {
+            
+              setSearchIds(response.data.ids);
+              setIsLoading(false)
+           
+            } else {
+              setSearchIds(null);
+              setIsLoading(false)
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            setSearchIds(null);
+            setIsLoading(false)
+          });
+        }
+        else if(name ==""){
+          setIsSearched(false)
+          setIsLoading(false)
+        }
+      }
+      
+      function updateContact(id:string){
+        axios.put(`${apiUrl}/edit/${id}`, {
+          id: form._id,
+          name:form.name,
+          phoneNo:form.phoneNo,
+          email:form.email
+        })
+        .then((response)=>{
+         setContacts(response.data.contacts)
+         setIsLoading(false)
+        })
+         .catch(function (error) {
+          console.log(error);
+        });
+      
+      
+      }
+      useEffect(()=>{
+      getAllContacts();
+      
+      }, [showForm])
+      
+      
+      
+      useEffect(()=>{
+      setTimeout(() => {
+       searchContact(searchName);
+      }, 1000);
+      
+      }, [searchName]) 
+      
+      function setFormData(id:string){
+        const find  = contacts?.find((cur)=> id === cur._id)
+        setForm({
+          _id: id,
+          name:find?.name as string,
+          phoneNo:find?.phoneNo as string,
+          email:find?.email as string,
+      
+        })
+      }
+      function createContact(formData:FormData){
+        
+      const name = formData.get("name")
+      const phoneNo = formData.get("phoneNo")
+      const email = formData.get("email")
+      
+      axios.post(`${apiUrl}/create`, {
+          name: name,
+          phoneNo: phoneNo,
+          email:email
+        })
+        .then(function (response) {
+          setContacts(response.data.contacts)
+          setIsLoading(false)
+      
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      
+      setShowForm(false)
+      }
+      
+    return {
+      contacts,
+    showForm, setShowForm,
+    setFormData,
+    isEdit, setIsEdit,
+    searchName, setSearchName,
+    editId, setEditId,
+    searchIds,
+    isSearched,
+    isLoading, setIsLoading,
+    form, setForm,
+    createContact, 
+    updateContact,
+    deleteContact,
+    // ... export everything else your UI needs
+  };
+}

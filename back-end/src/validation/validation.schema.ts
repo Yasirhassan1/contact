20a3290dac69d5aad7dd72cde7376b72eedf4de1
Yasirhassan1 <yasuhassan7896@gmail.com>
2026-01-22 23:11:40 +0500

    fixed

--- back-end/src/validation/validation.schema.ts
+++ back-end/src/validation/validation.schema.ts
@@ -1,11 +1,11 @@
 import z from "zod";
-export const email = z.string().email({message: "Invalid email"})
-export const password = z.string().min(8, "Password should be atleast 8 character").max(16, "Password should be less than 16 characters")
+export const EmailSchema = z.string().email({message: "Invalid email"})
+export const PasswordSchema = z.string().min(8, "Password should be atleast 8 character").max(16, "Password should be less than 16 characters")
 
   
 
 
 export const LoginValidationSchema = z.object({
-email: email,
-password: password
+email: EmailSchema,
+password: PasswordSchema
 })
